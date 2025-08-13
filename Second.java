8507e9fdc7f2b8daee9e7c2f115ff9493e0974f8
shashikala <padskala2015@gmail.com>
2025-08-13 10:47:16 +0530

    final

--- Second.java
+++ Second.java
@@ -1,19 +1,15 @@
-// public class Main{
-//     public static void main(String[] args) {
-//         System.out.println("Hello world");
-
-//     }
-
-// }
-public class Main{
+public class Second {
+   
     private String fname ="John";
     private String lname="Doe...";
     private String email ="john@doe.com";
 private int age=24;
 public static void main (String[] args){
-    Main myobj = new Main();
+   Second myobj = new Second();
     System.out.println("Name: " +myobj.fname +" "+myobj.lname);
     System.out.println("Email: " +myobj.email);
     System.out.println("Age: " +myobj.age);
 }
 } 
+
+
