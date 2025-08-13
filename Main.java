e805c2f4007adbb7ca7994db90c4b31a829ac44d
shashikala <padskala2015@gmail.com>
2025-08-13 11:10:59 +0530

    final

--- Main.java
+++ Main.java
@@ -5,15 +5,33 @@
 //     }
 
 // }
-public class Main{
-    private String fname ="John";
-    private String lname="Doe...";
-    private String email ="john@doe.com";
-private int age=24;
+// public class Main{
+//     private String fname ="John";
+//     private String lname="Doe...";
+//     private String email ="john@doe.com";
+// private int age=24;
+// public static void main (String[] args){
+//     Main myobj = new Main();
+//     System.out.println("Name: " +myobj.fname +" "+myobj.lname);
+//     System.out.println("Email: " +myobj.email);
+//     System.out.println("Age: " +myobj.age);
+// }
+// } 
+ class Person{
+    protected String fname ="John";
+   protected String lname="Doe...";
+   protected String email ="john@doe.com";
+   protected int age=24;
+ }
+   class Student extends Person {
+ int graduationYear = 2016;
+
 public static void main (String[] args){
-    Main myobj = new Main();
+    Student myobj = new Student();
     System.out.println("Name: " +myobj.fname +" "+myobj.lname);
     System.out.println("Email: " +myobj.email);
     System.out.println("Age: " +myobj.age);
+    System.out.println("Graduation Year: " +myobj.graduationYear);
 }
 } 
+ 
